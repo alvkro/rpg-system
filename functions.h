f600106d015456566acd2db1840463b6f3f787ef
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-10-16 21:02:12 -0300

    Fiz correções de bugs, adicionei duas funções e um banco de items

--- functions.h
+++ functions.h
@@ -4,13 +4,15 @@
 #include "structs.h"
 using namespace std;
 
+// Colocar uma biblioteca para randomizar os ataques (seria uma adição legal...)
+
 void showJogador(const Jogador& player) {
-    printf("Nome: %s", player.Nome);
+    printf("Nome: %s", player.Nome.c_str());
     printf("Vida: %iHP", player.Vida);
 }
 
 void showInimigo(const Inimigo& enemy) {
-    printf("Inimigo: %s", enemy.Nome);
+    printf("Inimigo: %s", enemy.Nome.c_str());
     printf("Vida: %iHP", enemy.Vida);
 }
 
@@ -29,18 +31,50 @@ void CreateItem(Item& item) { // Tem que achar uma maneira de salvar esses items
 }
 
 Item SelectItem(const Jogador& jogador) {
+    int Escolha;
     if (jogador.Inventario.empty()) {
         cout << "Inventário vazio!\n";
-        return {"", 0}; // Item sentinela!
+        return {"", 0}; 
     }
+    else {
+        for (int i = 0; i < jogador.Inventario.size(); i++) {
+            cout << i + 1 << ". " << jogador.Inventario[i].Nome << " (Poder: " << jogador.Inventario[i].Poder << ")\n";
+        }
+        while(true) {
+            cout << "Escolha um item:";
+            cin >> Escolha;
+            if(Escolha >= 1 && Escolha <= jogador.Inventario.size()) {
+                break;
+            }
+            else {
+                cout << "Item Inválido! Tente novamente\n";
+            }
+        }
+    }
+    return jogador.Inventario[Escolha - 1];
+}
 
-    int Escolha;
-    for (int i = 0; i < jogador.Inventario.size(); i++) {
-        cout << i + 1 << ". " << jogador.Inventario[i + 1].Nome << " (Poder: " << jogador.Inventario[i].Poder << ")\n";
-    }
-    printf("Selecione um item do seu inventário: \n");
-    // Escolha = 
-    /*if (Escolha < jogador.Inventario.size()) {
-        return 
-    }*/
+int AtaqueInimigo(Inimigo& enemy, Jogador& player) {
+    cout << player.Nome << " atacou " << enemy.Nome << "!\n";
+    return enemy.Vida -= player.Ataque;
+}
+
+int InimigoAtaque(Inimigo& enemy, Jogador& player) {
+    cout << enemy.Nome << " atacou " << player.Nome << "!\n";
+    return player.Vida -= enemy.Ataque;
+}
+
+vector <Item> BancoDeItens() {
+    vector <Item> Items;
+
+    Item espada_curta = {"Espada Curta", 10, TipoDeItem::Espada};
+    Item pocao_pequena = {"Pocao Pequena", 20, TipoDeItem::Pocao};
+    Item escudo_madeira = {"Escudo de Madeira", 5, TipoDeItem::Escudo};
+    Item adaga_ferro = {"Adaga de Ferro", 8, TipoDeItem::Espada};
+    // Adicionar mais items?
+
+    Items.push_back(espada_curta);
+    Items.push_back(pocao_pequena);
+    Items.push_back(escudo_madeira);
+    Items.push_back(adaga_ferro);
 }
